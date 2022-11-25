ea31d17bf16a5a0232fbf00395e39aaf2361efbd
ayaoihbi <ao2398@nyu.edu>
2022-11-25 10:01:19 +0800

    Initial commit

---
+++ blank-P5-template/sketch.js
@@ -0,0 +1,14 @@
+console.log("my script is loaded successfully")
+
+
+
+function setup(){
+    let cnv = createCanvas(200,200);
+    cnv.parents("canvasContainer")
+
+    
+    background(100,20,230)
+}
+function draw(){
+
+}
